cda3545131182ad88e89abfae356ac7942d08f4a
Wojciech Tałałaj <bearinabox@Bear-in-a-Mac.local>
2020-10-16 00:54:31 +0200

    Proof-of-concept versions of Login and Products

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -1,27 +1,67 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback, useState } from 'react';
+import type { FormEvent } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+
+import { authService } from 'services';
 
 import { AppRoute } from 'routing/AppRoute.enum';
+import { useIsLoggedIn } from 'hooks/use-is-logged-in';
 
 export const Login = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const isLoggedIn = useIsLoggedIn();
+
+  const performLogin = async () => {
+    try {
+      setLoading(true);
+      await authService.signIn({ username, password });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onLoginRequest = (event: FormEvent) => {
+    event.preventDefault();
+    performLogin();
+  };
+
+  if (isLoggedIn) {
+    return <Redirect to={AppRoute.home} />;
+  }
+
   return (
     <>
       <Link to={AppRoute.home}>Products page</Link>
       <h2>Login</h2>
-      <form>
+      <form onSubmit={onLoginRequest}>
         <div>
           <label>
             username:
-            <input name="username" />
+            <input
+              name="username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
           </label>
         </div>
         <div>
           <label>
             password:
-            <input name="password" type="password" />
+            <input
+              name="password"
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
           </label>
         </div>
-        <button type="submit">submit</button>
+        <button type="submit" disabled={loading}>
+          submit
+        </button>
       </form>
     </>
   );
--- src/app/products/Products.tsx
+++ src/app/products/Products.tsx
@@ -1,13 +1,28 @@
+import { useObservable } from 'hooks/use-observable';
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import { AppRoute } from 'routing/AppRoute.enum';
+import { authService, productsService } from 'services';
+import { Products as Model } from 'services/products/types';
 
 export const Products = () => {
+  const products: Model | Error | null = useObservable(
+    productsService.products$,
+    null
+  );
+  if (products == null) {
+    return <p>Loading</p>;
+  }
+  if (products instanceof Error) {
+    return <p>Error: ${products.message}</p>;
+  }
+  if (products.items.length === 0) {
+    return <p>No items</p>;
+  }
   return (
     <>
       <h2>Products page</h2>
-      <Link to={AppRoute.login}> Login </Link>
+      <p>{JSON.stringify(products)}</p>
+      <button onClick={() => authService.signOut()}>Logout</button>
     </>
   );
 };
