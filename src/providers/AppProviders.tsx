791b51beb3bf1464a7ee44a9cc15965689024244
Wojciech Tałałaj <bearinabox@Bear-in-a-Mac.local>
2020-10-16 00:51:43 +0200

    Added a provider for MaterialUI theme

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
 
-import { AppProvidersProps } from './AppProviders.types';
+import theme from 'theme';
 
-export const AppProviders = ({ children }: AppProvidersProps) => <Router>{children}</Router>;
+export const AppProviders: React.FC = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <Router>{children}</Router>
+  </ThemeProvider>
+);
